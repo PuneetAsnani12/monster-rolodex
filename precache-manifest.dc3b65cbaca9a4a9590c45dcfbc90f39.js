f08ac78c08ae621a4a2560e2f25373e4a53182cd
PuneetAsnani12 <asnani.puneet@gmail.com>
2020-03-18 00:20:30 +0530

    Updates

---
+++ precache-manifest.dc3b65cbaca9a4a9590c45dcfbc90f39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a448f78989800bc6b470de3db7b74d8",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "8593a85bde65f0bf451b",
+    "url": "/monster-rolodex/static/css/main.a29b8459.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monster-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8593a85bde65f0bf451b",
+    "url": "/monster-rolodex/static/js/main.b5722524.chunk.js"
+  },
+  {
+    "revision": "82aa35cf669de029c632",
+    "url": "/monster-rolodex/static/js/runtime-main.0d2831a3.js"
+  }
+]);
